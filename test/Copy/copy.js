1393d31710f8241f25457aa5bf24071d0e43b9a9
gxlydlyf <x1k1l1@163.com>
2023-11-17 20:46:31 +0800

    ss-off-by: gxlydlyf <x1k1l1@163.com>

--- test/Copy/copy.js
+++ test/Copy/copy.js
@@ -1,14 +1,48 @@
-function copyToClipboard(text) {
+function copyToClipboard(text, callback) {
+    if (typeof callback !== 'function') {
+        callback = function () {
+
+        }
+    }
+    callback = callback.bind(null);
+    var error;
+    var Elog = function (error) {
+        if (window.console && window.console.error) {
+            window.console.error(error);
+        }
+    }
     if (window.clipboardData && window.clipboardData.setData) {
         // For IE5
         window.clipboardData.setData('Text', text);
+    } else if (document.execCommand) {
+        if (document.body) {
+            var input = document.createElement("input");
+            input.value = text;
+            document.body.appendChild(input);
+            input.select();
+            input.setSelectionRange(0, input.value.length);
+            document.execCommand('Copy');
+            document.body.removeChild(input);
+            callback('success', text);
+        } else {
+            error = "Please wait until the body is loaded before executing.";
+            // window.console.log("请等待body加载完成之后再执行。");
+            Elog(error);
+            callback('error', error);
+        }
+    } else if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText("")
+            .then(() => {
+                // console.log('文本成功写入剪贴板');
+                callback('success', text);
+            })
+            .catch((error) => {
+                callback('error', error);
+            });
     } else {
-        // For modern browsers
-        var textarea = document.createElement('textarea');
-        textarea.value = text;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
+        // Clipboard API is not supported
+        error = "Clipboard API is not supported";
+        Elog(error);
+        callback('error', error);
     }
 }
