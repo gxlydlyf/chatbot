7b5d931b71a2d4c87d591463d7a1c4a205a3d007
gxlydlyf <x1k1l1@163.com>
2023-11-29 20:02:28 +0800

    ss-off-by: gxlydlyf <x1k1l1@163.com>

--- js/setting-config.js
+++ js/setting-config.js
@@ -60,7 +60,7 @@ function SettingConfigConstructor() {
             }
             var BUL = [
                 {
-                    'domain': 'tud5ihkrrr.freet.top/api/openai/v1/chat/completions',//postapi.lbbai.cc
+                    'domain': 'c1.glf.one/api/tud5ihkrrr.freet.top/api/openai/v1/chat/completions',//postapi.lbbai.cc
                     'encryption': false,
                     'description': '这是默认的 源地址 ，是免费的。',
                     'headers': {
@@ -70,16 +70,26 @@ function SettingConfigConstructor() {
                     'id': 1
                 },
                 {
+                    'domain': 'tud5ihkrrr.freet.top/api/openai/v1/chat/completions',//postapi.lbbai.cc
+                    'encryption': false,
+                    'description': '这是默认的 源地址 ，是免费的。',
+                    'headers': {
+                        'Origin': 'https://8162403981.ai701.live',
+                        'Referer': 'https://8162403981.ai701.live/'
+                    },
+                    'id': 2
+                },
+                {
                     'domain': 'U2FsdGVkX19cvRBxUkOPFmmsJIhzsZrVe4os0m9IKaQ=',
                     'encryption': true,
                     'description': '这是一个普通的 源地址 ，需要输入密码以解开。',
-                    'id': 2
+                    'id': 3
                 },
                 {
                     'domain': 'api.openai.com/v1/chat',
                     'encryption': false,
                     'description': '这是OpenAI官方的接口地址，需要使用境外网络以及自己的 api key 以使用。',
-                    'id': 3
+                    'id': 4
                 }
             ]
             if (id === false) {
