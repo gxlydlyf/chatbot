bf75127c509009e95c12cc96a0c98828a2bee134
gxlydlyf <x1k1l1@163.com>
2023-12-02 21:11:30 +0800

    ss-off-by: gxlydlyf <x1k1l1@163.com>

--- js/pwa.js
+++ js/pwa.js
@@ -3,43 +3,52 @@ $(document).ready(function () {
     try {
         if (window.navigator && ('serviceWorker' in window.navigator)) {
             // 浏览器支持 Service Worker
-            var deferredPrompt = {
-                prompt: function () {
-                    PWA_web_app_installBTN.children("span").text("失败");
-                    setTimeout(function () {
-                        PWA_web_app_installBTN.children("span").text("应用");
-                    }, 1000)
-                }
-            };
+            var deferredPrompt = undefined;
+            var appInstalled = false;
             window.addEventListener('beforeinstallprompt', function (e) {
                 e.preventDefault();
 
                 deferredPrompt = e;
             });
+            window.addEventListener('appinstalled', (e) => {
+                appInstalled = true;
+            });
+            if (deferredPrompt === undefined) {
+                if (appInstalled === true) {
+                    PWA_web_app_installBTN.click(function () {
+                        window.open("web+chatgpt://index")
+                    })
+                } else {
+                    PWA_web_app_installBTN.remove();
+                }
 
-            // 当用户点击安装提示按钮时，调用以下方法来安装应用
-            PWA_web_app_installBTN.click(function () {
-                deferredPrompt.prompt();
+            } else {
 
-                deferredPrompt.userChoice.then(function (choiceResult) {
-                    if (choiceResult.outcome === 'accepted') {
-                        console.log('用户接受 A2HS 提示');
-                        PWA_web_app_installBTN.children("span").text("成功");
-                        setTimeout(function () {
-                            PWA_web_app_installBTN.children("span").text("应用");
-                        }, 1000)
-                    } else {
-                        console.log('用户忽略了 A2HS 提示');
-                        PWA_web_app_installBTN.children("span").text("失败");
-                        setTimeout(function () {
-                            PWA_web_app_installBTN.children("span").text("应用");
-                        }, 1000)
-                    }
+                // 当用户点击安装提示按钮时，调用以下方法来安装应用
+                PWA_web_app_installBTN.click(function () {
+                    deferredPrompt.prompt();
 
-                    deferredPrompt = null;
-                });
+                    deferredPrompt.userChoice.then(function (choiceResult) {
+                        if (choiceResult.outcome === 'accepted') {
+                            console.log('用户接受 A2HS 提示');
+                            PWA_web_app_installBTN.children("span").text("成功");
+                            setTimeout(function () {
+                                PWA_web_app_installBTN.children("span").text("应用");
+                            }, 1000)
+                        } else {
+                            console.log('用户忽略了 A2HS 提示');
+                            PWA_web_app_installBTN.children("span").text("失败");
+                            setTimeout(function () {
+                                PWA_web_app_installBTN.children("span").text("应用");
+                            }, 1000)
+                        }
 
-            })
+                        // deferredPrompt = null;
+                    });
+
+                })
+            }
+
         } else {
             // 浏览器不支持 Service Worker
             PWA_web_app_installBTN.remove();
