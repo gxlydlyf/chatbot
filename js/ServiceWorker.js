7f2e7d0c7a44cb30c403c8fe77a33b86ef6e7578
gxlydlyf <x1k1l1@163.com>
2023-12-03 13:10:18 +0800

    ss-off-by: gxlydlyf <x1k1l1@163.com>

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -1,35 +1,9 @@
-//如果此文件引入时产生错误，说明不支持
-const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register("/sw.js", {
-                scope: "/",
-            });
-            if (registration.installing) {
-                console.log("正在安装 Service worker");
-            } else if (registration.waiting) {
-                console.log("已安装 Service worker installed");
-            } else if (registration.active) {
-                console.log("激活 Service worker");
-            }
-        } catch (error) {
-            console.error(`注册失败：${error}`);
-        }
-    }
-};
-
-// …
-
-registerServiceWorker();
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1");
-    await cache.addAll(resources);
-};
-
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        addResourcesToCache([
-            "/js/jquery2/"
-        ]),
-    );
-});
+//if 代码块进行特性检测测试，以确保在尝试注册 service worker 之前，该特性是被支持的。
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function (registration) {
+            console.log('Service Worker 注册成功:', registration);
+        }, function (error) {
+            console.log('Service Worker 注册失败:', error);
+        })
+}
---
+++ sw.js
@@ -0,0 +1,32 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('ChatGPTFreeCache').then(function (cache) {
+            return cache.addAll([
+                'js/html5shiv.min.js',
+                'js/jquery2/jquery-1.8.2.js',
+                'js/jquery2/jquery-ui.js',
+                'js/jquery2/jquery.ui.all.css',
+                'js/jquery.min.js',
+                'js/jquery.easing.min.js',
+                'js/jquery.easing.compatibility.js',
+                'js/ServiceWorker.js',
+                'js/json2.js',
+                'js/crypto-js.min.js',
+                'js/marked/marked.min.js',
+                'js/marked/highlight/index.umd.js',
+                'js/marked/highlight/highlight.min.js',
+                'js/marked/highlight/languages/go.min.js',
+                'js/marked/highlight/styles/a11y-dark.min.css',
+                'js/marked/linkify/index.umd.js',
+                'js/showdown/dist/showdown.js',
+                'js/SyntaxHighlighter.load.js',
+                'js/marked/marked.load.js',
+                'js/download.compatible.js',
+                'js/js.cookie.min.js',
+                'js/copy.js',
+                'index.html'
+                // 添加其他需要缓存的文件
+            ]);
+        })
+    );
+});
